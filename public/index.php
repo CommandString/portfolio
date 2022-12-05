655eadf07df61c7e91f9b7e10678ea12e3fddd1c
Command_String <bmxmikey2@gmail.com>
2022-12-05 06:42:27 +0000

    Removed all unneeded code

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 
-use CommandString\Cookies\Cookie;
-use CommandString\Pdo\Driver;
 use CommandString\Router\Environment as Env;
 use CommandString\Router\Router;
 
@@ -16,7 +14,7 @@ $router->setNamespace("Router\Routes");
 //#######################//
 //   SETUP ENVIRONMENT   //
 //#######################//
-$env = new Env("../private/env.json");
+$env = new Env(false);
 
 //################//
 //   SETUP TWIG   //
@@ -24,21 +22,5 @@ $env = new Env("../private/env.json");
 $env->twig = new \Twig\Environment((new \Twig\Loader\FilesystemLoader("../private/views")), [
     "cache" => ($env->cache_templates === true) ? "../private/cache" : false
 ]);
-
-//###################//
-//   SETUP COOKIES   //
-//###################//
-// $env->cookie = new Cookie((new \CommandString\CookieEncryption\Encryption(Env::get()->cookies->passphrase, Env::get()->cookies->method)));
-
-//######################//
-//   SETUP PDO DRIVER   //
-//######################//
-// $driver = (new Driver(true))
-//     ->withUsername($env->mysql->username)
-//     ->withPassword($env->mysql->password)
-//     ->withHost($env->mysql->host)
-//     ->withDatabase($env->mysql->database)
-//     ->connect()
-// ;
 
 require_once "../private/routes/Routes.php";
